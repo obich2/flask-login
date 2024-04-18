1efb6d4fc7099a127a6311aa5722e9e64d63a850
obich <g.shchet@gmail.com>
2024-04-18 23:25:14 +0300

    -

--- login.py
+++ login.py
@@ -1,11 +1,11 @@
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, login_required, logout_user
 from flask import Flask, redirect, render_template
 from data import db_session
 from data.users import User
 from forms.loginform import LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['SECRET_KEY'] = '9vTgySlnihdzBGrf'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -13,8 +13,17 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
+    print(user_id)
     db_sess = db_session.create_session()
+    print(user_id)
     return db_sess.query(User).get(user_id)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -24,6 +33,7 @@ def login():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
+            print(form.remember_me.data)
             login_user(user, remember=form.remember_me.data)
             return redirect("/authorized")
         return render_template('login.html',
